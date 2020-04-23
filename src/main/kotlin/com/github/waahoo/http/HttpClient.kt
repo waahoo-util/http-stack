0a683e160c3883f331676d6332d0ef638cf7e6bc
waahoo <waahoo@outlook.com>
2020-04-23 08:10:21 +0800

    0.0.9

--- src/main/kotlin/com/github/waahoo/http/HttpClient.kt
+++ src/main/kotlin/com/github/waahoo/http/HttpClient.kt
@@ -9,18 +9,20 @@ import java.net.CookieManager
 import java.net.CookiePolicy
 import java.net.CookieStore
 
-const val userAgent =
+const val UserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
 
-fun makeClient(cookieStore: CookieStore? = null, logLevel: HttpLoggingInterceptor.Level = NONE) =
-  OkHttpClient.Builder().also {
+fun makeClient(
+  cookieStore: CookieStore? = null, logLevel: HttpLoggingInterceptor.Level = NONE,
+  userAgent: String = UserAgent
+): OkHttpClient {
+  return OkHttpClient.Builder().let {
     if (cookieStore != null) {
       val cookieMgr = CookieManager(cookieStore, CookiePolicy.ACCEPT_ALL)
       CookieHandler.setDefault(cookieMgr)
       it.cookieJar(JavaNetCookieJar(cookieMgr))
     }
-  }
-    .addNetworkInterceptor { chain ->
+    it.addNetworkInterceptor { chain ->
       val req = chain.request()
       chain.proceed(
         req.newBuilder()
@@ -28,13 +30,19 @@ fun makeClient(cookieStore: CookieStore? = null, logLevel: HttpLoggingIntercepto
           .build()
       )
     }
-    .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(logLevel))
-    .build()
+    it.addNetworkInterceptor(HttpLoggingInterceptor().setLevel(logLevel))
+    it.build()
+  }
+
+}
 
 lateinit var client: OkHttpClient
 
-fun initClient(cookieStore: CookieStore? = null, logLevel: HttpLoggingInterceptor.Level = NONE) {
-  client = makeClient(cookieStore, logLevel)
+fun initClient(
+  cookieStore: CookieStore? = null, logLevel: HttpLoggingInterceptor.Level = NONE,
+  userAgent: String = UserAgent
+) {
+  client = makeClient(cookieStore, logLevel, userAgent)
 }
 
 fun closeClient() {
